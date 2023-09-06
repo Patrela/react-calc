656baeb61c88aa104c104e644bfc6ffc29bbe95b
Paty Varela <patrela@hotmail.com>
2023-09-06 15:08:12 -0500

    ph-pages needs java script - install react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App';
-
+/*********   */
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
+/*********   */
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(  
+  <React.StrictMode>    
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
+/*
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider
+    router={router}
+    fallbackElement={<BigSpinner />}
+  />
+);
+
+*/
